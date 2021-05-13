ee3972a34bdd255b144b0d26ed4458efff8f6ff2
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-13 18:49:20 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogSingleComponent } from './Components/blog-single/blog-single.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { IndexComponent } from './Components/index/index.component';
+import { LoginComponent } from './Components/login/login.component';
+import { PricingComponent } from './Components/pricing/pricing.component';
+import { RecordVideoComponent } from './Components/record-video/record-video.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "", component : IndexComponent },
+  { path : "pricing", component : PricingComponent },
+  { path : "blog", component : BlogComponent },
+  { path : "login", component : LoginComponent },
+  { path : "blog-single", component : BlogSingleComponent },
+  { path : "record-video", component : RecordVideoComponent },
+  { path : "register", component : RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './Components/index/index.component';
+import { PricingComponent } from './Components/pricing/pricing.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { LoginComponent } from './Components/login/login.component';
+import { BlogSingleComponent } from './Components/blog-single/blog-single.component';
+import { RecordVideoComponent } from './Components/record-video/record-video.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    PricingComponent,
+    BlogComponent,
+    LoginComponent,
+    BlogSingleComponent,
+    RecordVideoComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
