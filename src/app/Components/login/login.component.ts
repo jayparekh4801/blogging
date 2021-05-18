1e80a25f1a911dad66d5c27c9883be2035dff0c9
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-18 12:57:41 +0530

    validaion finished

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginData } from 'src/app/Datamodels/loginData';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
+	loginData : LoginData = {
+		email : "",
+		password : ""
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	loginForm = new FormGroup({
+		email : new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
+		password : new FormControl('', [Validators.required])
+	});
+
+	get email() {
+		return this.loginForm.get('email') as FormControl
+	}
+
+	get password() {
+		return this.loginForm.get('password') as FormControl;
+	}
 
 }
